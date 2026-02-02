0c93dca2bc36a8689a022d768ef1d6755cd40fc2
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2026-02-03 00:18:44 +0100

    feat(imgw): add DTOs with handled IMGW API typos in property name

--- src/main/java/pl/czyzlowie/modules/imgw/client/dto/ImgwHydroResponse.java
+++ src/main/java/pl/czyzlowie/modules/imgw/client/dto/ImgwHydroResponse.java
@@ -1,4 +1,46 @@
 package pl.czyzlowie.modules.imgw.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImgwHydroResponse {
+    @JsonProperty("id_stacji")
+    private String stationId;
+    @JsonProperty("stacja")
+    private String station;
+    @JsonProperty("rzeka")
+    private String river;
+    @JsonProperty("wojewodztwo")
+    private String province;
+    @JsonProperty("lon")
+    private String longitude;
+    @JsonProperty("lat")
+    private String latitude;
+    @JsonProperty("stan_wody")
+    private String waterLevel;
+    @JsonProperty("stan_wody_data_pomiaru")
+    private String waterLevelDate;
+    @JsonProperty("temperatura_wody")
+    private String waterTemperature;
+    @JsonProperty("temperatura_wody_data_pomiaru")
+    private String waterTemperatureDate;
+    @JsonProperty("przelyw")
+    private String waterFlow;
+    @JsonProperty("przeplyw_data_pomiaru")
+    private String waterFlowDate;
+    @JsonProperty("zjawisko_lodowe")
+    private String icePhenomenon;
+    @JsonProperty("zjawisko_lodowe_data_pomiaru")
+    private String icePhenomenonDate;
+    @JsonProperty("zjawisko_zarastania")
+    private String overgrowthPhenomenon;
+    @JsonProperty("zjawisko_zarastania_data_pomiaru")
+    private String overgrowthPhenomenonDate;
 }
--- src/main/java/pl/czyzlowie/modules/imgw/client/dto/ImgwMeteoResponse.java
+++ src/main/java/pl/czyzlowie/modules/imgw/client/dto/ImgwMeteoResponse.java
@@ -1,4 +1,35 @@
 package pl.czyzlowie.modules.imgw.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImgwMeteoResponse {
+
+    @JsonProperty("id_stacji")
+    private String stationId;
+    @JsonProperty("stacja")
+    private String station;
+    @JsonProperty("data_pomiaru")
+    private String measurementDate;
+    @JsonProperty("godzina_pomiaru")
+    private String measurementHour;
+    @JsonProperty("temperatura")
+    private String temperature;
+    @JsonProperty("predkosc_wiatru")
+    private String windSpeed;
+    @JsonProperty("kierunek_wiatru")
+    private String windDirection;
+    @JsonProperty("wilgotnosc_wzgledna")
+    private String relativeHumidity;
+    @JsonProperty("suma_opadu")
+    private String totalPrecipitation;
+    @JsonProperty("cisnienie")
+    private String pressure;
 }
