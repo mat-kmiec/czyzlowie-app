75dadd7d2ebbfb3eaea0ae6a7baa7e92338d306e
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2026-02-04 23:16:25 +0100

    doc(ImgwClient): add JavaDoc

--- src/main/java/pl/czyzlowie/modules/imgw/client/ImgwClient.java
+++ src/main/java/pl/czyzlowie/modules/imgw/client/ImgwClient.java
@@ -8,6 +8,12 @@ import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
+/**
+ * Client for fetching data from external APIs, specifically designed to interact with the IMiGW API.
+ * This class uses a {@link RestClient} to make HTTP GET requests and retrieve data, which is then deserialized
+ * into the specified list of objects of a given type.
+ * Additionally, it handles logging and error management, ensuring robust error handling for API communication.
+ */
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -15,6 +21,14 @@ public class ImgwClient {
 
     private final RestClient restClient = RestClient.create();
 
+    /**
+     * Fetches a list of data from the given URL using the provided response type for deserialization.
+     *
+     * @param <T> the type of elements in the list
+     * @param url the URL to fetch the data from
+     * @param responseType the parameterized type reference for deserialization
+     * @return the list of deserialized objects; if the data cannot be retrieved, returns an empty list
+     */
     public <T> List<T> fetchList(String url, ParameterizedTypeReference<List<T>> responseType) {
         try {
             List<T> result = restClient.get()
