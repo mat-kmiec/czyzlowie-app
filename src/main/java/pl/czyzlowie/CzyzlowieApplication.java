db36feee4cd15a6327f11c91f45a454809f97745
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2026-02-04 02:04:18 +0100

    feat(imgw): implement automated data synchronization with configurable intervals

--- src/main/java/pl/czyzlowie/CzyzlowieApplication.java
+++ src/main/java/pl/czyzlowie/CzyzlowieApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ConfigurationPropertiesScan("pl.czyzlowie.modules.imgw.config")
 public class CzyzlowieApplication {
 
---
+++ src/main/java/pl/czyzlowie/modules/imgw/scheduler/ImgwDataSceduler.java
@@ -0,0 +1,88 @@
+package pl.czyzlowie.modules.imgw.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pl.czyzlowie.modules.imgw.service.ImgwFetchFacade;
+
+/**
+ * A scheduler for triggering periodic fetching of meteorological, hydrological, and synoptic data.
+ *
+ * This class uses Spring's {@code @Scheduled} annotation to execute data-fetching tasks at fixed intervals,
+ * as defined by external configuration properties. The data-fetching operations are delegated to the
+ * {@code ImgwFetchFacade}, which handles the orchestrated fetching and processing of the respective data types.
+ *
+ * Each scheduled task logs its start, success, or failure, with detailed exception information when errors occur.
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ImgwDataSceduler {
+
+    private final ImgwFetchFacade fetchFacade;
+
+    /**
+     * Schedules the automatic fetching of meteorological data at a fixed rate.
+     *
+     * This method triggers the meteorological data fetch operation by invoking {@code fetchMeteo}
+     * on the {@code fetchFacade}. It logs the start, success, or failure of the fetching process.
+     * Any exceptions occurring during the operation are caught and logged with the error details.
+     *
+     * The execution interval is defined and configured externally using the property
+     * {@code imgw.scheduler.meteo-rate}.
+     */
+    @Scheduled(fixedRateString = "${imgw.scheduler.meteo-rate}")
+    public void scheduleMeteoFetch(){
+        log.info("Auto-Fetching: METEO start");
+        try{
+            fetchFacade.fetchMeteo();
+            log.info("Auto-Fetching: METEO success");
+        } catch (Exception e){
+            log.error("Auto-Fetching: METEO failed", e);
+        }
+    }
+
+    /**
+     * Schedules the automatic fetching of hydrological data at a fixed rate.
+     *
+     * This method triggers the hydrological data fetch operation by invoking {@code fetchHydro}
+     * on the {@code fetchFacade}. It logs the start, success, or failure of the fetching process.
+     * Any exceptions occurring during the operation are caught and logged with the error details.
+     *
+     * The execution interval is defined and configured externally using the property
+     * {@code imgw.scheduler.hydro-rate}.
+     */
+    @Scheduled(fixedRateString = "${imgw.scheduler.hydro-rate}")
+    public void scheduleHydroFetch(){
+        log.info("Auto-Fetching: HYDRO start");
+        try{
+            fetchFacade.fetchHydro();
+            log.info("Auto-Fetching: HYDRO success");
+        } catch (Exception e){
+            log.error("Auto-Fetching: HYDRO failed", e);
+        }
+    }
+
+    /**
+     * Schedules the automatic fetching of SYNOP (synoptic) data at a fixed rate.
+     *
+     * This method triggers the fetch operation by invoking {@code fetchSynop} on the
+     * {@code fetchFacade}. It logs the start, success, or failure of the fetching process.
+     * Any exceptions occurring during the operation are caught and logged with error details.
+     *
+     * The execution interval is defined and configured externally using the property
+     * {@code imgw.scheduler.synop-rate}.
+     */
+    @Scheduled(fixedRateString = "${imgw.scheduler.synop-rate}")
+    public void scheduleSynopFetch(){
+        log.info("Auto-Fetching: SYNOP start");
+        try{
+            fetchFacade.fetchSynop();
+            log.info("Auto-Fetching: SYNOP success");
+        } catch (Exception e){
+            log.error("Auto-Fetching: SYNOP failed", e);
+        }
+    }
+}
