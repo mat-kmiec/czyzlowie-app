0303e84a88e6e3001ddf5523a2bd7e530aad2fcd
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2026-02-11 23:50:14 +0100

    feat(entity): create entity for moon stations and global data

---
+++ src/main/java/pl/czyzlowie/modules/moon/entity/Enums/MoonPhaseType.java
@@ -0,0 +1,12 @@
+package pl.czyzlowie.modules.moon.entity.Enums;
+
+public enum MoonPhaseType {
+    NEW_MOON,
+    WAXING_CRESCENT,
+    FIRST_QUARTER,
+    WAXING_GIBBOUS,
+    FULL_MOON,
+    WANING_GIBBOUS,
+    LAST_QUARTER,
+    WANING_CRESCENT;
+}
