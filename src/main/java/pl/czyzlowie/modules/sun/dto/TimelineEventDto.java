7c9bce40cb47dc3fd703da52449a28f946138220
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2026-03-04 15:09:56 +0100

    feat(sun): add JavaDoc

--- src/main/java/pl/czyzlowie/modules/sun/dto/TimelineEventDto.java
+++ src/main/java/pl/czyzlowie/modules/sun/dto/TimelineEventDto.java
@@ -3,6 +3,14 @@ package pl.czyzlowie.modules.sun.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * Data Transfer Object representing an individual event in a timeline, typically used to
+ * describe solar events or related occurrences in a structured, detailed manner.
+ *
+ * This class encapsulates details about the event's time range, title, description,
+ * and various visual aspects including icon and styling properties for customization.
+ * It also indicates whether the event occurs during the golden hour.
+ */
 @Getter
 @Builder
 public class TimelineEventDto {
